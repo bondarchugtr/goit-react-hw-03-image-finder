6aa57eafe19eac79f17d1d76c1479c30e2f4f03f
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-12-08 14:05:36 +0200

    +

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "modern-normalize/modern-normalize.css";
 function App() {
   return (
     <div className="App">
